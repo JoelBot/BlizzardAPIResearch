9d0381df33a09ddbb6ab39a7e0d0d164c40b2dee
Joel Lonneker <joel.lonneker@gmail.com>
2017-04-05 19:50:11 -0700

    restructured home.js to work more React-like and did some horrendous UI tests for using API objects

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import moment from 'moment'
 // import Nav from '../components/Nav'
 
 
@@ -7,7 +8,12 @@ import { Link } from 'react-router'
 class Home extends Component {
   constructor(props) {
     super(props)
-
+    this.updateState = this.updateState.bind(this)
+    this.state = {
+      name: '',
+      thumbnail: '',
+      lastUpdated: '',
+    }
   }
   componentWillMount() {
     console.log('Console Will Mount...')
@@ -16,16 +22,33 @@ class Home extends Component {
   componentDidMount() {
     fetch('https://us.api.battle.net/wow/character/fenris/tomes?locale=en_US&apikey=vp6vgjjs48wb4c567qd556e8zkvgzzwb')
     .then(response => response.json())
-    .then(response => console.log(response))
+    // .then(console.log(response))
+    .then(this.updateState)
   }
+  updateState(response) {
 
+    console.log(response)
+    console.log("hello")
+      this.setState({
+          name: response.name,
+          thumbnail: 'http://render-api-us.worldofwarcraft.com/static-render/us/' + response.thumbnail,
+          // http://render-api-us.worldofwarcraft.com/static-render/us/fenris/44/129984556-profilemain.jpg
+          lastUpdated: moment(response.lastModified).fromNow()
+      })
+
+    }
     render() {
         return  <div>
                     <div id="homePage" className="row">
-                        <div className="col-sm-12 text-center">
-                            <h1>Welcome to Character Diffster!</h1>
-                            <h3>You did stuff, here is the proof: check the console -></h3>
-                        </div>
+                        <div className="col-sm-9 text-center">
+                            <h2>Welcome to Character Diffster!</h2>
+                            <h3>You did stuff, here is the proof: check the console -> </h3>
+                          </div>
+                            <div className="col-sm-3">
+                              <div> <img src={this.state.thumbnail} alt="" /></div>
+                              <div>  Name: {this.state.name}</div>
+                              <div>  Last Updated: {this.state.lastUpdated}</div>
+                          </div>
                     </div>
                 </div>
 
--- components/TestAPIDump.js
+++ components/TestAPIDump.js
@@ -3,16 +3,26 @@ import React, { Component } from 'react'
 class APIDump extends Component {
   constructor(props) {
     super(props)
-
+    this.updateState = this.updateState.bind(this)
+    this.state = {
+      name: '',
+      thumbnail: '',
+      lastUpdated: '',
+    }
   }
   componentWillMount() {
-    console.log('Console Will Mount...')
+    console.log('Console Will Mount... doom')
 
   }
   componentDidMount() {
     fetch('https://us.api.battle.net/wow/character/fenris/tomes?locale=en_US&apikey=vp6vgjjs48wb4c567qd556e8zkvgzzwb')
     .then(response => response.json())
-    .then(console.log(response))
+    // .then(console.log(response))
+    .then(this.updateState)
+  }
+  updateState(response) {
+    console.log(response)
+    console.log("hello")
   }
   render() {
       return  <div>
