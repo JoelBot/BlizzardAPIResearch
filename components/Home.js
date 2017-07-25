2e98f43a81039c562b97b0303f5faaa2c6b42c13
Joel Lonneker <joel.lonneker@gmail.com>
2017-07-24 20:57:52 -0700

    some state and use of configs to hide API key

--- components/Home.js
+++ components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import config from '../config.js'
 import { Link } from 'react-router'
 // import Nav from '../components/Nav'
 
@@ -7,24 +8,44 @@ import { Link } from 'react-router'
 class Home extends Component {
   constructor(props) {
     super(props)
-
+    this.typing = this.typing.bind(this)
+    this.enter = this.enter.bind(this)
+    this.state = {
+      name: '',
+      serverName: 'fenris',
+      api: 'https://us.api.battle.net/wow/character/',
+      key: '?locale=en_US&apikey='
+    }
   }
   componentWillMount() {
-    console.log('Console Will Mount...')
+    console.log('Console Will Mount...') 
 
   }
   componentDidMount() {
-    fetch('https://us.api.battle.net/wow/character/fenris/tomes?locale=en_US&apikey=vp6vgjjs48wb4c567qd556e8zkvgzzwb')
-    .then(response => response.json())
-    .then(response => console.log(response))
-  }
 
+  }
+typing (e) {
+  this.setState({
+    name: e.target.value
+  })
+}
+  enter(e) {
+    if (e.key === 'Enter') {
+          fetch(this.state.api + this.state.serverName + '/' + this.state.name + this.state.key + MY_KEY)
+          .then(response => response.json())
+          .then(response => console.log(response))
+    }
+  }
     render() {
         return  <div>
                     <div id="homePage" className="row">
                         <div className="col-sm-12 text-center">
                             <h1>Welcome to Character Diffster!</h1>
-                            <h3>You did stuff, here is the proof: check the console -></h3>
+                              <br />
+                              <br />
+                            <label htmlFor="charName">Character Name:</label>
+                            <span> </span>
+                            <input type="text" id="charName" placeholder="Fabulon" value={this.state.name} onChange={this.typing} onKeyPress={this.enter}></input>
                         </div>
                     </div>
                 </div>
